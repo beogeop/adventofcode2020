74afb7ecee554cc0a3bab4752c23e5978ed592d6
Joo Young Choi <38427759+Chejuyeong@users.noreply.github.com>
2021-07-26 11:21:55 +0900

    Update Day02.py

--- Day02/Day02.py
+++ Day02/Day02.py
@@ -2,13 +2,19 @@ file = open("input.txt", "r")
 list = []
 list1 = []
 list2 = []
+list3 = []
 
 for i in file:
     list.append(i)
 
 for i in list:
     list1.append(i.split())
-    for j in list1:
-        list2.append(int(j[0]).split("-"))
+    
+for j in list1:
+    list2.append(j[0].split("-"))
 
-print(list2)
+for i in list1:
+    x = i[1].replace(":", "")
+    list3.append(x)
+
+print(list3)
